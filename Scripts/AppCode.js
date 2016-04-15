75ee6d45460194452703afc49df7b30de655d918
Tomer Gandler <Tomer Gandler>
2016-04-15 12:17:45 +0300

    Initial commit

---
+++ Scripts/AppCode.js
@@ -0,0 +1,254 @@
+﻿/// <reference path="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js" />
+/// <reference path="jquery.cookie.js" />
+/// <reference path="Helper.js" />
+
+// ------------------------------ <INIT> -------------------------------- //
+
+var servers = {};
+// servers[index] {ip, name, {properties}, notify}
+// properties { version, online, max, motd, { players }, favicon }
+// player { name, icon }
+
+var count;
+$.cookie.defaults = { path: '/', expires: 2555 };
+
+$(document).ready(function () {
+
+    count = $.cookie('count');
+    if (count == undefined) {
+        $.cookie('count', 0);
+        count = 0;
+    }
+
+    if (count != 0)
+        $('#description').hide();
+
+    if ($.cookie('permission') != undefined) {
+        if (Notification.permission !== "granted")
+            Notification.requestPermission();
+        $.removeCookie('permission');
+    }
+
+    // Share
+
+    if (location.search != "") {
+        var qstring = location.search.toString().substring(1).split(';');
+
+        if (qstring.length == 2)
+            addServer(qstring[0], qstring[1]);
+
+        location.search = "";
+    }
+
+    // Generate Page
+
+    var gen = "";
+    var actual = 0;
+    if (count != 0) {
+        for (var i = 1; i <= count; i++) {
+            var serverName = $.cookie('serverName' + i);
+            var serverIP = $.cookie('serverIP' + i);
+            var notified = $.cookie('serverNotif' + i); // 0 / 1
+            if (serverName != undefined) {
+                servers["" + i] = {
+                    "name": serverName,
+                    "ip": serverIP,
+                    "notif": notified
+                };
+
+                actual++;
+                gen += '<div class="server" id="' + i + '">';
+                gen += genServerHTML("" + i);
+                gen += '</div>';
+            }
+        }
+
+        if (actual == 0) {
+            $.cookie('count', 0);
+            location.reload();
+        }
+
+        $('#container').html(gen);
+
+        pingall();
+    }
+
+
+    // ------------------------------ </INIT> -------------------------------- //
+
+    // ------------------------------ <LISTENERS> -------------------------------- //
+
+    // Clicks, Buttons
+
+    $('#addServer').click(function () {
+        var serverName = $('#serverName').val();
+        var serverIP = $('#serverIP').val();
+
+        if (serverName == "" || serverIP == "") {
+            alert("Insert a valid name and IP.");
+            return;
+        }
+
+        addServer(serverName, serverIP);
+    });
+
+    $('.removeServer').click(function () {
+        removeServer(this.value, true);
+    });
+
+    $('.eatAllCookies').click(function () {
+        for (var i = 1; i <= count; i++)
+            removeServer(i, false);
+        $.removeCookie('count');
+        location.reload();
+        return "Yum";
+    });
+
+    $('.refresh').click(function () {
+        var index = this.value;
+        rePing("" + index);
+    });
+
+    $('.openAdd').click(function () {
+        $('#add').show('fade');
+        $('#shader').show('fade');
+    });
+
+    $('.openShare').click(function () {
+        $('#share').show('fade');
+        $('#shader').show('fade');
+
+        $('#shareLink').val(this.value);
+    });
+
+    $('.toggleNotif.off').click(function () {
+        var index = this.value;
+        $.cookie('serverNotif' + index, 1);
+        if (Notification.permission !== "granted")
+            $.cookie('permission', true);
+        location.reload(true);
+    });
+
+    $('.toggleNotif.on').click(function () {
+        var index = this.value;
+        $.cookie('serverNotif' + index, 0);
+        location.reload(true);
+    });
+
+    $('.openEdit').click(function () {
+        $('#edit').show('fade');
+        $('#shader').show('fade');
+        var index = this.value;
+        $('#editName').val($.cookie('serverName' + index));
+        $('#editIP').val($.cookie('serverIP' + index));
+        $('#editSave').val(index);
+    });
+
+    $('#editSave').click(function () {
+        edit(this.value);
+    });
+
+    $('#edit .close').click(function () {
+        $('#edit').hide('fade');
+        $('#shader').hide('fade');
+    });
+
+    $('#share .close').click(function () {
+        $('#share').hide('fade');
+        $('#shader').hide('fade');
+    });
+
+    $('#add .close').click(function () {
+        $('#add').hide('fade');
+        $('#shader').hide('fade');
+    });
+
+    $('#shader').click(function () {
+        $('#add').hide('fade');
+        $('#shader').hide('fade');
+        $('#edit').hide('fade');
+        $('#share').hide('fade');
+    });
+
+    // Mouse Enters
+
+    $('.server').mouseenter(function () {
+        showLabel(this.id);
+        $('#' + this.id + ' .actionBar').show('fade');
+    });
+
+    $('.server').mouseleave(function () {
+        hideLabel(this.id);
+        $('#' + this.id + ' .actionBar').hide('fade');
+    });
+
+    // Special
+
+    $(window).resize(function () {
+        $('#add, #edit, #share').css({
+            position: 'fixed',
+            left: ($(window).width() - $('#add, #edit, #share').outerWidth()) / 2,
+            top: ($(window).height() - $('#add, #edit, #share').outerHeight()) / 2
+        });
+
+    });
+
+    $(window).resize();
+
+    $(document).mousemove(function (e) {
+        $('.follow').offset({
+            left: e.pageX,
+            top: e.pageY + 20
+        });
+    });
+
+    // ------------------------------ </LISTENERS> -------------------------------- //
+
+    // ------------------------------ <INTERVALS> -------------------------------- //
+
+    setInterval(function () {
+        for (var x in servers)
+            rePing("" + x);
+    }, 300000);
+
+    // ------------------------------ </INTERVALS> -------------------------------- // 
+
+});
+
+function rePing(index) {
+    var prev = $.extend(true, servers[index]);
+    resetServer(index);
+    ping(index, prev);
+}
+
+function update(index, prev) {
+    if (servers[index]['notif'] == 1) {
+        var openPrev = prev['properties'] != undefined;
+        var openCurr = servers[index]['properties'] != undefined;
+
+        if (openPrev && openCurr) {
+            var playersPrev = prev['properties']['players'];        
+            var playersCurr = servers[index]['properties']['players'];
+
+            for (var x in playersCurr) {
+                var name = playersCurr[x]['name'];
+                if (playersPrev[name] == undefined) {
+                    var msg = name + " Has Joined " + servers[index]['name'];
+                    var icon = playersCurr[x]['icon'];
+                    notify(msg, { 'icon': icon });
+                }
+            }
+        }
+
+        if (openPrev && !openCurr) {
+            var msg = servers[index]['name'] + " went Offline.";
+            notify(msg);
+        }
+
+        if (!openPrev && openCurr) {
+            var msg = servers[index]['name'] + " Is Online.";
+            var icon = servers[index]['properties']['favicon'];
+            notify(msg, { 'icon': icon });
+        }
+    }
+}
---
+++ Scripts/Helper.js
@@ -0,0 +1,189 @@
+﻿/// <reference path="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js" />
+/// <reference path="jquery.cookie.js" />
+/// <reference path="AppCode.js" />
+
+function genServerHTML(index) {
+    var gen = "";
+    gen += '<h1>' + servers[index]["name"] + '</h1>';
+    gen += '<div class="favicon" style="width:64px; height:64px;"></div>';
+    gen += '<div class="motd">Pinging...</div>';
+    gen += '<div class="players"></div>';
+    gen += '<label for="' + index + '" class="playersList"></label>';
+    gen += '<div class="version"></div>';
+    gen += '<div class="actionBar">';
+    gen += '    <button class="option removeServer" value="' + index + '"></button>';
+    gen += '    <button class="option openEdit" value="' + index + '"></button>';
+    gen += '    <button class="option openShare" value="' + document.URL + "?" + servers[index]["name"] + ";" + servers[index]["ip"] + '"></button>';
+    if (servers[index]['notif'] == 0)
+        gen += '<button class="option toggleNotif off" value="' + index + '"></button>';
+    else
+        gen += '<button class="option toggleNotif on" value="' + index + '"></button>';
+    gen += '    <button class="option refresh" value="' + index + '"></button>';
+    gen += '</div>';
+
+    return gen;
+}
+
+function resetServer(index) {
+    $('#' + index + ' .motd').html("Pinging...");
+    $('#' + index + ' .players').html("");
+    $('#' + index + ' .playersList').html("");
+    $('#' + index).removeClass('open');
+    $('#' + index).removeClass('closed');
+
+    servers["" + index]['properties'] = undefined;
+}
+
+function showLabel(index) {
+    $('label[for="' + index + '"]').show();
+    $('label[for="' + index + '"]').addClass("follow");
+}
+
+function hideLabel(index) {
+    $('label[for="' + index + '"]').hide();
+    $('label[for="' + index + '"]').removeClass("follow");
+}
+
+function addServer(name, ip) {
+    count++;
+    $.cookie('count', count);
+    $.cookie("serverName" + count, name);
+    $.cookie("serverIP" + count, ip);
+    $.cookie("serverNotif" + count, 0);
+    location.reload(true);
+}
+
+
+function removeServer(index, reload) {
+    $.removeCookie('serverName' + index);
+    $.removeCookie('serverIP' + index);
+    $.removeCookie('serverNotif' + index);
+    if (reload)
+        location.reload(true);
+}
+
+function edit(index) {
+    var serverName = $('#editName').val();
+    var serverIP = $('#editIP').val();
+    $.cookie('serverName' + index, serverName);
+    $.cookie('serverIP' + index, serverIP);
+    location.reload(true);
+}
+
+// ------ NOTIFICATION -------- //
+
+function notify(msg, options) {
+    if (!Notification) {
+        alert('Notifications are supported in modern versions of Chrome, Firefox, Opera and Firefox.');
+        return;
+    }
+
+    if (Notification.permission !== "granted")
+        Notification.requestPermission();
+
+    var icon = 'http://mutepackgenerator.webege.com/res/ping.png';
+    if (options != undefined)
+        icon = options["icon"];
+
+    var notification = new Notification('Minecraft Server Ping!', {
+        icon: icon,
+        body: msg
+    });
+
+    notification.onclick = function () {
+        window.open(document.location);
+    };
+}
+
+function colorForamtting(str) {
+    var sp = str.split('§');
+    var gen = sp[0];
+    var end = "";
+    for (i = 1; i < sp.length; i++) {
+        gen += "<span class='cf" + sp[i].charAt(0) + "'>";
+        gen += sp[i].substring(1);
+        end += "</span>";
+    }
+    gen.replace("\n", "</br>");
+    gen += end;
+
+    return gen;
+}
+
+// ------ PING -------- //
+
+var mcping = "https://mcapi.ca/query/";
+var playerHead = "https://crafatar.com/avatars/";
+
+function pingall() {
+    for (var index in servers)
+        ping("" + index);
+}
+
+function ping(index, prev) {
+    var xmlhttp = new XMLHttpRequest();
+
+    var url = mcping + servers[index]['ip'] + "/info";
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var arr = JSON.parse(xmlhttp.responseText);
+
+            if (arr['status']) {
+                var players = {};
+
+                for (var x in arr['sample']) {
+                    players["" + arr['sample'][x]['name']] = {
+                        "name": arr['sample'][x]['name'],
+                        "icon": playerHead + arr['sample'][x]['name']
+                    };
+                }
+
+                servers[index]['properties'] = {
+                    "version": arr['version'],
+                    "online": arr['players']['online'],
+                    "max": arr['players']['max'],
+                    "motd": arr['motd'],
+                    "players": players,
+                    "favicon": mcping + servers[index]['ip'] + "/icon"
+                };
+            }
+
+            translate(index);
+            if (prev !== undefined)
+                update(index, prev);
+        }
+    };
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+
+function translate(index) {
+    if (servers[index]['properties'] == undefined) {
+        $("#" + index + " .motd").html("server offline.");
+        $("#" + index + " .playersList").html("Server Empty.");
+        $("#" + index).addClass("closed");
+        return;
+    }
+
+    $("#" + index).addClass("open");
+    $("#" + index + " .favicon").css("background-image", "url(" + servers[index]['properties']['favicon'] + ")");
+    $("#" + index + " .motd").html("" + colorForamtting(servers[index]['properties']['motd']));
+    $("#" + index + " .players").html("" + servers[index]['properties']['online'] + " / " + servers[index]['properties']['max']);
+    $("#" + index + " .version").html("" + servers[index]['properties']['version']);
+
+    var gen = "<table>";
+    for (var x in servers[index]['properties']['players']) {
+        gen += "<tr>";
+        gen += "<td><img width='40px' src='" + servers[index]['properties']['players'][x]['icon'] + "'/></td>";
+        gen += "<td>" + servers[index]['properties']['players'][x]['name'] + "</td>";
+        gen += "</tr>";
+    }
+    gen += "</table>";
+
+    if (gen === "<table></table>")
+        gen = "Server Empty.";
+
+    $("#" + index + " .playersList").html("" + gen);
+}
